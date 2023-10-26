46ec366f13f97c22a3895ed949d37afb3c328176
Fayazshaik2207 <89535985+Fayazshaik2207@users.noreply.github.com>
2023-10-26 13:47:28 +0000

    container config done

---
+++ js_basics/variables.js
@@ -0,0 +1,8 @@
+const Pi=3.14
+let name="fayaz"
+var account_id=1234
+console.table([Pi,name,account_id])
+
+// Pi=2.14 not allowed because const variable cant be changed
+
+// prefer not to use var because of block scope and functional scope
